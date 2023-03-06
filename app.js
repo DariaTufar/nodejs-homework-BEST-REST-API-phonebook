e06c86e6ef5c45e826fb02f0beed638a214b3f88
Daria Shenkevych <daria.shenkevych@gmail.com>
2023-03-06 18:18:55 +0200

    hw -04-auth completed
    working features

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const cors = require('cors')
 
 require("dotenv").config()
 const contactsRouter = require('./routes/api/contacts')
+const authRouter = require("./routes/api/users");
 
 const app = express()
- 
+
+
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
@@ -14,11 +16,12 @@ app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
 
-app.use('/api/contacts', contactsRouter)
+app.use('/api/contacts', contactsRouter);
+app.use('/api/auth', authRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
-})
+})  
 
 app.use((err, req, res, next) => {
   const { status = 500, message = "Some server error, we are working on it" } = err;
---
+++ controllers/users.js
@@ -0,0 +1,82 @@
+const { HttpError } = require("../helpers");
+
+const { ctrlWrapper } = require("../helpers/ctrlWrapper.js");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+require("dotenv").config();
+const { SECRET_KEY } = process.env;
+
+const { User } = require("../models/user");
+
+// ======= Sign-Up =====================
+
+async function signup(req, res) {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    throw HttpError(409, "Email already in use");
+  }
+  const hashedPassword = await bcrypt.hash(password, 10);
+  //   const compareResult1 = await bcrypt.compare(password, result);
+  const newUser = await User.create({
+    ...req.body,
+    password: hashedPassword,
+  });
+  res.status(201).json({
+    email: newUser.email,
+    password: newUser.password,
+  });
+}
+// =======Log in  =====================
+async function login(req, res) {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw HttpError(401, "Email or password is wrong");
+  }
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  if (!passwordCompare) {
+    throw HttpError(401, "Email or password wrong");
+  }
+  const payload = {
+    id: user._id,
+  };
+
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+  await User.findByIdAndUpdate(user._id, { token });
+  res.status(200).json({
+    data: {
+      token,
+      user: { email: user.email, subscription: user.subscription },
+    },
+  });
+}
+
+//  ========== Show current user ========show  email details
+const getCurrent = async (req, res) => {
+  const { email, subscription } = req.user;
+
+  res.json({
+    email,
+    subscription,
+  });
+};
+
+// =========  Log Out ============================
+const logout = async (req, res) => {
+  const { _id } = req.user;
+  await User.findByIdAndUpdate(_id, { token: "" });
+
+  res.json({
+    message: "No Content",
+  });
+};
+
+module.exports = {
+  signup: ctrlWrapper(signup),
+  login: ctrlWrapper(login),
+  getCurrent: ctrlWrapper(getCurrent),
+  logout: ctrlWrapper(logout),
+};
